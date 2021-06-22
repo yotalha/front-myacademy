4a816cde53d98e6ec05ec13e440ade34c5b3f823
yotalha <talha.coder9@gmail.com>
2021-06-22 15:10:11 +0500

    components folder and login component

---
+++ src/Components/Login/copyright.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+import { Link } from '@material-ui/core';
+
+
+
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="#">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default Copyright;
