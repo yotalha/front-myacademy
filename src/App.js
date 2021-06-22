81d2d54ac89810038d0d6b2cce94a026c05ee2fb
yotalha <talha.coder9@gmail.com>
2021-06-23 00:29:33 +0500

    components to containers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Signup from './Components/Signup';
+import Signup from './Containers/Signup';
 
 
 function App() {
