a880be8da53f353c73a61e4836af9b17b05ca3a4
yotalha <talha.coder9@gmail.com>
2021-06-22 15:27:43 +0500

    signup copyright and index

--- src/Components/Signup/copyright.js
+++ src/Components/Signup/copyright.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 import { Link } from '@material-ui/core';
 
-
-
 const Copyright = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
